8f27cf3db03e12ad8d5d746337974e7abaf7808d
Yosef Mare <yosefmare108@gmail.com>
2024-12-04 15:52:47 +0200

    rename role filed in students and teachers schemas to isTheacher

--- school_AI_system/backend/schemas/studentScheema.js
+++ school_AI_system/backend/schemas/studentScheema.js
@@ -2,8 +2,9 @@ import { Schema, model } from "mongoose";
 
 const StudentSchema = new Schema({
   name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true, unique: true },
-  role: { type: String, required: true},
+  isTeacher: { type: Boolean, default: false},
   studentClass: { type: String, required: true },
   grade: { type: Number, required: true },
 })
--- school_AI_system/backend/schemas/teachersSchema.js
+++ school_AI_system/backend/schemas/teachersSchema.js
@@ -4,7 +4,7 @@ const TeacherSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true },
+  isTeacher: { type: Boolean, required: true},
 });
 
 export default model("teacher", TeacherSchema)
