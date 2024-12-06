bd412d87e8d6250d05509c7b4ff3713f3bddfc44
Yosef Mare <yosefmare108@gmail.com>
2024-12-06 15:24:45 +0200

    create addStudent asyncThunk and builder

--- school_AI_system/frontend/school_ai_system/src/app/redux/feacures/teachers/teachersAsyncTunks.ts
+++ school_AI_system/frontend/school_ai_system/src/app/redux/feacures/teachers/teachersAsyncTunks.ts
@@ -1,11 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Teacher, TeacherResponse } from "./teacherSlice";
+import { getCookie } from "@/utils/getCooies";
 
 export interface LoginFormData {
   email?: string | null;
   password: string;
   role?: string;
+}
+export interface AddStudentFormData{
+name: string;
+password: string;
+studentClass: string;
 }
 
 export const teacherLogin = createAsyncThunk<Teacher, LoginFormData>(
@@ -26,6 +32,29 @@ export const teacherLogin = createAsyncThunk<Teacher, LoginFormData>(
       console.error(error.response?.data || error.message);
 
       // Use rejectWithValue for error handling
+      return rejectWithValue(error.response?.data || "Failed to login");
+    }
+  }
+);
+
+export const addStudent = createAsyncThunk<Teacher, AddStudentFormData>(
+  "teachers/login",
+  async (formData, { rejectWithValue }) => {
+    try {
+      // API call
+      const token = getCookie('token');
+            const headers = token ? { Authorization: `Bearer ${token}` } : {};
+
+      const response = await axios.post<TeacherResponse>(
+        "http://localhost:8000/api/teachers/addStudent",
+        formData,
+        { headers}
+      );
+
+      console.log(response.data);
+      return response.data.teacher;
+    } catch (error: any) {
+      console.log(error.response?.data || error.message);
       return rejectWithValue(error.response?.data || "Failed to login");
     }
   }
--- school_AI_system/frontend/school_ai_system/src/app/redux/feacures/teachers/teachersExtraReducers.ts
+++ school_AI_system/frontend/school_ai_system/src/app/redux/feacures/teachers/teachersExtraReducers.ts
@@ -1,5 +1,5 @@
 import { ActionReducerMapBuilder } from '@reduxjs/toolkit';
-import { teacherLogin } from "./teachersAsyncTunks";
+import { addStudent, teacherLogin } from "./teachersAsyncTunks";
 import { InitialState } from "./teacherSlice";
 import Cookies from "js-cookie";
 
@@ -15,6 +15,21 @@ export const teacherLoginExtraReducer = (builder: ActionReducerMapBuilder<Initia
 
     })
     .addCase(teacherLogin.rejected, (state, action) => {
+      state.error = 'failed';
+      state.error = action.error.message;
+    });
+}
+export const addStidentExtraReducer = (builder: ActionReducerMapBuilder<InitialState>) => {
+  builder
+  .addCase(addStudent.pending, (state) => {
+    state.loading = true;
+  })
+  .addCase(addStudent.fulfilled, (state, action) => {
+      state.loading = false;
+      state.value = action.payload
+
+    })
+    .addCase(addStudent.rejected, (state, action) => {
       state.error = 'failed';
       state.error = action.error.message;
     });
