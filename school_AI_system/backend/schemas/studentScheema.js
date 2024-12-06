db41c5769426173aa05be5d77cd2aa47c3fe59aa
Yosef Mare <yosefmare108@gmail.com>
2024-12-06 15:13:29 +0200

    delete sum fileds

--- school_AI_system/backend/schemas/studentScheema.js
+++ school_AI_system/backend/schemas/studentScheema.js
@@ -2,11 +2,11 @@ import { Schema, model } from "mongoose";
 
 const StudentSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, default: process.env.STUDENT_PLACEHOLDER_EMAIL},
+  email: { type: String, default: process.env.STUDENT_PLACEHOLDER_EMAIL }, // No `unique` constraint here
   password: { type: String, required: true, unique: true },
-  role: { type: String, default: "student"},
+  role: { type: String, default: "student" },
   studentClass: { type: String, required: true },
-  grade: { type: Number, required: true },
-})
+  grade: { type: Number, default: 0 },
+});
 
 export default model("student", StudentSchema)
